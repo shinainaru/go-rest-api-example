790448e8efd414ebb670c6334e478e94f79bb06d
shinainaru <90768067+shinainaru@users.noreply.github.com>
2022-04-13 20:09:55 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,79 +1,25 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
+// run with: env PORT=8081 go run http-server.go
 
-	"github.com/gorilla/mux"
+import (
+    "fmt"
+    "log"
+    "net/http"
+    "os"
 )
 
-type Response struct {
-	Persons []Person `json:"persons"`
-}
-
-type Person struct {
-	Id        int    `json:"id"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-}
-
 func main() {
-	log.Println("starting API server")
-	//create a new router
-	router := mux.NewRouter()
-	log.Println("creating routes")
-	//specify endpoints
-	router.HandleFunc("/health-check", HealthCheck).Methods("GET")
-	router.HandleFunc("/persons", Persons).Methods("GET")
-	http.Handle("/", router)
 
-	//start and listen to requests
-	http.ListenAndServe(":8080", router)
+    port := os.Getenv("PORT")
+    if port == "" {
+      log.Fatal("Please specify the HTTP port as environment variable, e.g. env PORT=8081 go run http-server.go")
+    }
 
-}
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+        fmt.Fprintf(w, "Hello World")
+    })
 
-func HealthCheck(w http.ResponseWriter, r *http.Request) {
-	log.Println("entering health check end point")
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "API is up and running")
-}
+    log.Fatal(http.ListenAndServe(":" + port, nil))
 
-func Persons(w http.ResponseWriter, r *http.Request) {
-	log.Println("entering persons end point")
-	var response Response
-	persons := prepareResponse()
-
-	response.Persons = persons
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	jsonResponse, err := json.Marshal(response)
-	if err != nil {
-		return
-	}
-
-	w.Write(jsonResponse)
-}
-
-func prepareResponse() []Person {
-	var persons []Person
-
-	var person Person
-	person.Id = 1
-	person.FirstName = "Issac"
-	person.LastName = "N"
-	persons = append(persons, person)
-
-	person.Id = 2
-	person.FirstName = "Albert"
-	person.LastName = "E"
-	persons = append(persons, person)
-
-	person.Id = 3
-	person.FirstName = "Thomas"
-	person.LastName = "E"
-	persons = append(persons, person)
-	return persons
 }
